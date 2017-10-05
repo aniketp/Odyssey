5312a50daaac15bfcb04bff84ce546bae6addf58
aniketp <aniketp@iitk.ac.in>
2017-10-05 16:49:13 +0530

    Added leaderboard

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -2,7 +2,16 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from treasure.models import *
 
-# Create your views here.
+
 def leaderboard(request):
-    pass
+    wings = UserProfile.objects.all().order_by('-score')
+
+    index = 1
+    for wing in wings:
+        wing.rank = index
+        wing.save()
+        index += 1
+
+    return render(request, 'leaderboard.html', context={'wings': wings})
