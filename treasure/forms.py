2e8d407f2c9c23ad3ceceae88978be7d576fc9ee
aniketp <aniketp@iitk.ac.in>
2017-10-07 04:28:39 +0530

    Added basu question model

--- treasure/forms.py
+++ treasure/forms.py
@@ -4,3 +4,8 @@ from .models import *
 
 class QuestionForm(forms.Form):
     answer = forms.CharField()
+
+
+class BasuForm1(forms.Form):
+    answer1 = forms.CharField(help_text='Answer 1')
+    answer2 = forms.CharField(help_text='Answer 2')
--- treasure/urls.py
+++ treasure/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='odyssey'),
     url(r'^question/(?P<pk>\d+)/$', views.questions, name='questions'),
-    url(r'^bonus-question/(?P<pk>\d+)/$', views.bonus_questions, name='bonus-questions')
+    url(r'^bonus-question/(?P<pk>\d+)/$', views.bonus_questions, name='bonus-questions'),
+    url(r'^basu-question1/(?P<pk>\d+)/$', views.basu_question1_2, name='basu-questions1_2')
 ]
